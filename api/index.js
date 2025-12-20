d1f704994275797e23a3ab2de70647ceba99d325
Dylan <70771914+ngoldbla@users.noreply.github.com>
2025-12-19 21:29:04 -0500

    Remove WhatsApp/Puppeteer and simplify deploy (#6)

--- api/index.js
+++ api/index.js
@@ -1,18 +1,5 @@
 module.exports = async function(app, io){
     require('./visitor')(app,io)
-
-    // WhatsApp module is optional - it requires Chromium and won't work on all platforms
-    if (process.env.ENABLE_WHATSAPP === 'true') {
-        try {
-            await require('./whatsapp')(app,io)
-        } catch (error) {
-            console.warn('WhatsApp module failed to load:', error.message)
-        }
-    } else {
-        console.log('WhatsApp module disabled (set ENABLE_WHATSAPP=true to enable)')
-    }
-
-    require('./chat')(app,io)
     require('./page')(app,io)
     require('./login')(app,io)
     require('./guard')(app,io)
